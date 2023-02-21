770156ca3163ae51d46d7e1c889c1ae646cc44bb
“Edward <eminaya49@gmail.com>
2023-02-21 14:12:15 -0600

    init

---
+++ store_item.rb
@@ -0,0 +1,11 @@
+item1 = { color: "blue", price: 20, weight: 10 }
+item2 = { color: "red", price: 100, weight: 20 }
+item3 = { color: "yellow", price: 50, weight: 40 }
+
+puts "This item is #{item1[:color]} and costs #{item1[:price]}"
+
+item1 = { :color => "blue", :price => 20, :weight => 10 }
+item2 = { :color => "red", :price => 100, :weight => 20 }
+item3 = { :color => "yellow", :price => 50, :weight => 40 }
+
+puts "This item is #{item1[:color]} and costs #{item1[:price]}"
