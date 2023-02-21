c403ec2c52437274a417f4c32da1ad5cced447b7
“Edward <eminaya49@gmail.com>
2023-02-21 14:14:56 -0600

    added comments for JavaScript and Ruby Syntax

--- store_item.rb
+++ store_item.rb
@@ -1,9 +1,11 @@
+# JavaScript Syntax
 item1 = { color: "blue", price: 20, weight: 10 }
 item2 = { color: "red", price: 100, weight: 20 }
 item3 = { color: "yellow", price: 50, weight: 40 }
 
 puts "This item is #{item1[:color]} and costs #{item1[:price]}"
 
+# Ruby Syntax
 item1 = { :color => "blue", :price => 20, :weight => 10 }
 item2 = { :color => "red", :price => 100, :weight => 20 }
 item3 = { :color => "yellow", :price => 50, :weight => 40 }
