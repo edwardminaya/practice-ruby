9c3f94a99ab514f7be5dfdb192957696be32b156
“Edward <eminaya49@gmail.com>
2023-02-22 16:06:06 -0600

    started debugging exercises

---
+++ debug.rb
@@ -0,0 +1,26 @@
+# Exercise 1: Print a message to a pirate
+puts "Hello!"
+greeting = gets.chomp()
+
+if greeting == "Arrr!"
+  puts "Go away, pirate."
+else
+  puts "Greetings, hater of pirates!"
+end
+
+# Exercise 2: Write a function to print when each person died in a sentence.
+dickens = ["Charles Dickens", "1870"]
+thackeray = ["William Thackeray", "1863"]
+trollope = ["Anthony Trollope", "1882"]
+hopkins = ["Gerard Manley Hopkins", "1889"]
+
+def died(array)
+  name = array[0]
+  year = array[1]
+  puts "#{name} died in #{year}."
+end
+
+puts died(dickens)
+puts died(thackeray)
+puts died(trollope)
+puts died(hopkins)
