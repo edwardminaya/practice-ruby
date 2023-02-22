a2be93f4cb3c15cd91a9f8dc0c80999cf0c2b195
“Edward <eminaya49@gmail.com>
2023-02-22 16:33:16 -0600

    completed exercises 1-4

--- debug.rb
+++ debug.rb
@@ -24,3 +24,30 @@ puts died(dickens)
 puts died(thackeray)
 puts died(trollope)
 puts died(hopkins)
+
+# Exercise 3: Print a message to a time traveler
+puts "Greetings! What is your year of origin?"
+origin = gets.chomp.to_i
+if origin < 1900
+  puts "That's the past!"
+elsif origin >= 1900 && origin <= 2020
+  puts "That's the present!"
+else
+  puts "That's the future!"
+end
+
+# Exercise 4: Make a class to store and display a person's info
+class Person
+  def initialize(firstname, lname)
+    @first_name = firstname
+    @last_name = lname
+  end
+
+  def to_s
+    @last_name + ", " + @first_name
+  end
+end
+
+tj = Person.new("Thomas", "Jefferson")
+puts tj.to_s
+puts tj
